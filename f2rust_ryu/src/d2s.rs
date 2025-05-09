030f56963da2e42fbfad1e67f7cb2a0c56892bf6
Philip Taylor <philip@zaynar.co.uk>
2025-05-09 12:48:31 +0100

    Delete unused code from f2rust_ryu

--- f2rust_ryu/src/d2s.rs
+++ f2rust_ryu/src/d2s.rs
@@ -34,51 +34,6 @@ pub const DOUBLE_BIAS: i32 = 1023;
 pub const DOUBLE_POW5_INV_BITCOUNT: i32 = 125;
 pub const DOUBLE_POW5_BITCOUNT: i32 = 125;
 
-#[cfg_attr(feature = "no-panic", inline)]
-pub fn decimal_length17(v: u64) -> u32 {
-    // This is slightly faster than a loop.
-    // The average output length is 16.38 digits, so we check high-to-low.
-    // Function precondition: v is not an 18, 19, or 20-digit number.
-    // (17 digits are sufficient for round-tripping.)
-    debug_assert!(v < 100000000000000000);
-
-    if v >= 10000000000000000 {
-        17
-    } else if v >= 1000000000000000 {
-        16
-    } else if v >= 100000000000000 {
-        15
-    } else if v >= 10000000000000 {
-        14
-    } else if v >= 1000000000000 {
-        13
-    } else if v >= 100000000000 {
-        12
-    } else if v >= 10000000000 {
-        11
-    } else if v >= 1000000000 {
-        10
-    } else if v >= 100000000 {
-        9
-    } else if v >= 10000000 {
-        8
-    } else if v >= 1000000 {
-        7
-    } else if v >= 100000 {
-        6
-    } else if v >= 10000 {
-        5
-    } else if v >= 1000 {
-        4
-    } else if v >= 100 {
-        3
-    } else if v >= 10 {
-        2
-    } else {
-        1
-    }
-}
-
 // A floating decimal representing m * 10^e.
 pub struct FloatingDecimal64 {
     pub mantissa: u64,
