792642a56d743b6aae6f2ed7bd4c218752232786
Philip Taylor <philip@zaynar.co.uk>
2025-05-09 15:25:57 +0100

    Improve formatted output of floats: fix rounding, non-finite values

--- f2rust_std/src/io/writer.rs
+++ f2rust_std/src/io/writer.rs
@@ -52,11 +52,62 @@ fn format_i(n: i32, w: usize, m: Option<usize>, plus: Option<bool>) -> Vec<u8> {
     }
 }
 
+fn format_edf_nonfinite(n: f64, w: usize) -> Option<Vec<u8>> {
+    let mut out = String::new();
+
+    // Match gfortran's behaviour for non-finite numbers
+    if n.is_infinite() {
+        if n < 0.0 {
+            out += "-";
+        }
+        if out.len() + 8 <= w {
+            out += "Infinity";
+        } else if out.len() + 3 <= w {
+            out += "Inf";
+        } else {
+            return Some(overflow(w));
+        }
+    } else if n.is_nan() {
+        if out.len() + 3 <= w {
+            out += "NaN";
+        } else {
+            return Some(overflow(w));
+        }
+    } else {
+        return None;
+    }
+
+    Some(Vec::from_iter(
+        repeat_n(b' ', w - out.len()).chain(out.into_bytes()),
+    ))
+}
+
 // TODO: This needs a lot more testing
 fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
-    let dec = f2rust_ryu::d2d(n);
+    if let Some(r) = format_edf_nonfinite(n, w) {
+        return r;
+    }
 
     let mut out = String::new();
+
+    let dec = f2rust_ryu::d2d(n);
+
+    // dec represents n as m*10^e
+    //
+    // d is the desired number of fractional digits, after the decimal point.
+    //
+    // If the number is <1.0, there is an optional "0" before the "."
+    // (unless there are no other digits, in which case it's mandatory).
+    // gfortran always includes this, *unless* the output would exceed width w,
+    // in which case it omits the "0".
+    //
+    // Some cases to consider:
+    //
+    //  m=12345 e=0 d=4 =>  "12345.0000"
+    //  m=12345 e=1 d=4 => "123450.0000" - these are easy, just output 'm' then some 0s
+    //
+    //  m=12345 e=-1 d=4 =>  "1234.5000" - just need to put the '.' and '0's in the right place
+    //  m=12345 e=-5 d=4 =>     "0.1235" - we need to round the number, and truncate
 
     if n < 0.0 {
         out += "-";
@@ -64,9 +115,12 @@ fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
         out += "+";
     }
 
-    let digits = dec.mantissa.to_string();
     if dec.exponent >= 0 {
-        out += &digits;
+        // Output the mantissa, *unless* this number is 0 in which case the leading 0 is
+        // omitted if it's not the only digit and would otherwise cause an overflow
+        if dec.mantissa != 0 || d == 0 || (dec.mantissa == 0 && out.len() + 2 + d <= w) {
+            out += &dec.mantissa.to_string();
+        }
 
         if out.len() + dec.exponent as usize + 1 + d > w {
             return overflow(w);
@@ -77,6 +131,15 @@ fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
         }
     } else {
         let nexp = (-dec.exponent) as usize;
+
+        // Round away from zero
+        let rounded_man = if nexp > d {
+            dec.mantissa + 5 * u64::pow(10, (nexp - d) as u32 - 1)
+        } else {
+            dec.mantissa
+        };
+
+        let digits = rounded_man.to_string();
         if digits.len() > nexp {
             // Last `-dec.exponent` digits are the fraction
             let (integer, fraction) = digits.split_at(digits.len() - nexp);
@@ -88,7 +151,6 @@ fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
                 return overflow(w);
             } else {
                 if fraction.len() > d {
-                    // TODO: round the last digit
                     out += &fraction[0..d];
                 } else {
                     out += fraction;
@@ -99,8 +161,9 @@ fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
             // Integer part is 0
             let fraction = digits;
 
-            // Add the optional `0.` unless that would cause us to overflow
-            if out.len() + 2 + d <= w {
+            // Add the optional `0.` unless that would cause us to overflow,
+            // unless it's the only digit in which case it's mandatory
+            if out.len() + 2 + d <= w || d == 0 {
                 out.push('0');
             }
             out.push('.');
@@ -114,7 +177,6 @@ fn format_f(n: f64, w: usize, d: usize, plus: Option<bool>) -> Vec<u8> {
                 out.extend(repeat_n('0', fill));
 
                 if fraction.len() > d - fill {
-                    // TODO: round the last digit
                     out += &fraction[0..d - fill];
                 } else {
                     out += &fraction;
@@ -139,6 +201,10 @@ fn format_ed(
         todo!("Ew.dEe not supported");
     }
 
+    if let Some(r) = format_edf_nonfinite(n, w) {
+        return r;
+    }
+
     let dec = f2rust_ryu::d2d(n);
 
     let mut out = String::new();
@@ -149,7 +215,19 @@ fn format_ed(
         out += "+";
     }
 
-    let fraction = dec.mantissa.to_string();
+    // Round away from zero
+    let man_digits = dec
+        .mantissa
+        .checked_ilog10()
+        .and_then(|i| Some(i + 1))
+        .unwrap_or(0);
+    let rounded_man = if man_digits > d as u32 {
+        dec.mantissa + 5 * u64::pow(10, man_digits - d as u32 - 1)
+    } else {
+        dec.mantissa
+    };
+
+    let fraction = rounded_man.to_string();
 
     let exp = if dec.mantissa == 0 {
         format!("{echar}+00")
@@ -172,7 +250,6 @@ fn format_ed(
         return overflow(w);
     } else {
         if fraction.len() > d {
-            // TODO: round the last digit
             out += &fraction[0..d];
         } else {
             out += &fraction;
@@ -185,81 +262,132 @@ fn format_ed(
     Vec::from_iter(repeat_n(b' ', w - out.len()).chain(out.into_bytes()))
 }
 
-#[test]
-fn test_f() {
-    // TODO: NaN, infinity?
-    // TODO: rounding
+#[cfg(test)]
+mod tests {
+    use super::*;
 
-    for (n, s) in [
-        (0.0, "    0.0000"),
-        (1.0, "    1.0000"),
-        (50.0, "   50.0000"),
-        (88000.0, "88000.0000"),
-        (888000.0, "**********"),
-        (-50.0, "  -50.0000"),
-        (1.1, "    1.1000"),
-        (0.11, "    0.1100"),
-        (0.011, "    0.0110"),
-        (0.0011, "    0.0011"),
-        (0.00011, "    0.0001"),
-        (0.000011, "    0.0000"),
-        (0.0000011, "    0.0000"),
-        // (5.55554, "    5.5554"),
-        // (5.55555, "    5.5556"),
-        // (0.00554, "    0.0054"),
-        // (0.00555, "    0.0056"),
-    ] {
-        assert_eq!(String::from_utf8_lossy(&format_f(n, 10, 4, None)), s);
-    }
+    #[test]
+    fn test_f() {
+        for (n, s) in [
+            (0.0, "    0.0000"),
+            (1.0, "    1.0000"),
+            (50.0, "   50.0000"),
+            (88000.0, "88000.0000"),
+            (888000.0, "**********"),
+            (-50.0, "  -50.0000"),
+            (1.1, "    1.1000"),
+            (0.11, "    0.1100"),
+            (0.011, "    0.0110"),
+            (0.0011, "    0.0011"),
+            (0.00011, "    0.0001"),
+            (0.000011, "    0.0000"),
+            (0.0000011, "    0.0000"),
+            (5.55554, "    5.5555"),
+            (5.55555, "    5.5556"),
+            (0.00554, "    0.0055"),
+            (0.00555, "    0.0056"),
+            (-5.55554, "   -5.5555"),
+            (-5.55555, "   -5.5556"),
+            (-0.00554, "   -0.0055"),
+            (-0.00555, "   -0.0056"),
+        ] {
+            assert_eq!(String::from_utf8_lossy(&format_f(n, 10, 4, None)), s);
+        }
 
-    for (n, w, d, s) in [
-        (0.1, 4, 1, " 0.1"),
-        (0.1, 3, 1, "0.1"),
-        (0.1, 2, 1, ".1"),
-        (0.1, 1, 1, "*"),
-        (9.94, 7, 1, "    9.9"),
-        // (9.95, 7, 1, "   10.0"),
-        (99999.94, 7, 1, "99999.9"),
-        // (99999.95, 7, 1, "*******"),
-    ] {
-        assert_eq!(String::from_utf8_lossy(&format_f(n, w, d, None)), s);
-    }
+        for (n, w, d, s) in [
+            (0.1, 4, 1, " 0.1"),
+            (0.1, 3, 1, "0.1"),
+            (0.1, 2, 1, ".1"),
+            (0.1, 1, 1, "*"),
+            (0.001, 4, 1, " 0.0"),
+            (0.001, 3, 1, "0.0"),
+            (0.001, 2, 1, ".0"),
+            (0.001, 1, 1, "*"),
+            (0.001, 3, 0, " 0."),
+            (0.001, 2, 0, "0."),
+            (0.001, 1, 0, "*"),
+            (0.0, 4, 1, " 0.0"),
+            (0.0, 3, 1, "0.0"),
+            (0.0, 2, 1, ".0"),
+            (0.0, 1, 1, "*"),
+            (0.0, 3, 0, " 0."),
+            (0.0, 2, 0, "0."),
+            (0.0, 1, 0, "*"),
+            (9.94, 7, 1, "    9.9"),
+            (9.95, 7, 1, "   10.0"),
+            (99999.94, 7, 1, "99999.9"),
+            (99999.95, 7, 1, "*******"),
+        ] {
+            assert_eq!(String::from_utf8_lossy(&format_f(n, w, d, None)), s);
+        }
 
-    assert_eq!(
-        String::from_utf8_lossy(&format_f(50.0, 10, 4, Some(true))),
-        "  +50.0000"
-    );
-}
-
-#[test]
-fn test_e() {
-    for (n, s) in [
-        (0.0, "  0.0000E+00"),
-        (1.0, "  0.1000E+01"),
-        (-1.0, " -0.1000E+01"),
-        (0.01, "  0.1000E-01"),
-        (50.0, "  0.5000E+02"),
-        (0.5e90, "  0.5000E+90"),
-        (0.5e100, " 0.5000E+100"),
-        (0.5e-90, "  0.5000E-90"),
-        (0.5e-100, " 0.5000E-100"),
-    ] {
         assert_eq!(
-            String::from_utf8_lossy(&format_ed('E', n, 12, 4, None, None)),
-            s
+            String::from_utf8_lossy(&format_f(50.0, 10, 4, Some(true))),
+            "  +50.0000"
         );
     }
 
-    for (n, w, d, s) in [
-        (0.1, 8, 1, " 0.1E+00"),
-        (0.1, 7, 1, "0.1E+00"),
-        (0.1, 6, 1, ".1E+00"),
-        (0.1, 5, 1, "*****"),
-    ] {
-        assert_eq!(
-            String::from_utf8_lossy(&format_ed('E', n, w, d, None, None)),
-            s
-        );
+    #[test]
+    fn test_e() {
+        for (n, s) in [
+            (0.0, "  0.0000E+00"),
+            (1.0, "  0.1000E+01"),
+            (-1.0, " -0.1000E+01"),
+            (0.01, "  0.1000E-01"),
+            (50.0, "  0.5000E+02"),
+            (0.5e90, "  0.5000E+90"),
+            (0.5e100, " 0.5000E+100"),
+            (0.5e-90, "  0.5000E-90"),
+            (0.5e-100, " 0.5000E-100"),
+            (0.55554, "  0.5555E+00"),
+            (0.55555, "  0.5556E+00"),
+            (-0.55554, " -0.5555E+00"),
+            (-0.55555, " -0.5556E+00"),
+            (0.99999, "  0.1000E+01"),
+        ] {
+            assert_eq!(
+                String::from_utf8_lossy(&format_ed('E', n, 12, 4, None, None)),
+                s
+            );
+        }
+
+        for (n, w, d, s) in [
+            (0.1, 8, 1, " 0.1E+00"),
+            (0.1, 7, 1, "0.1E+00"),
+            (0.1, 6, 1, ".1E+00"),
+            (0.1, 5, 1, "*****"),
+            (0.0, 8, 1, " 0.0E+00"),
+            (0.0, 7, 1, "0.0E+00"),
+            (0.0, 6, 1, ".0E+00"),
+            (0.0, 5, 1, "*****"),
+        ] {
+            assert_eq!(
+                String::from_utf8_lossy(&format_ed('E', n, w, d, None, None)),
+                s
+            );
+        }
+    }
+
+    #[test]
+    fn test_nonfinite() {
+        for (n, w, d, s) in [
+            (f64::INFINITY, 10, 1, "  Infinity"),
+            (f64::INFINITY, 8, 1, "Infinity"),
+            (f64::INFINITY, 7, 1, "    Inf"),
+            (f64::INFINITY, 2, 1, "**"),
+            (f64::NEG_INFINITY, 9, 1, "-Infinity"),
+            (f64::NEG_INFINITY, 8, 1, "    -Inf"),
+            (f64::NEG_INFINITY, 3, 1, "***"),
+            (f64::NAN, 4, 1, " NaN"),
+            (f64::NAN, 3, 1, "NaN"),
+            (f64::NAN, 2, 1, "**"),
+        ] {
+            assert_eq!(String::from_utf8_lossy(&format_f(n, w, d, None)), s);
+            assert_eq!(
+                String::from_utf8_lossy(&format_ed('E', n, w, d, None, None)),
+                s
+            );
+        }
     }
 }
 
